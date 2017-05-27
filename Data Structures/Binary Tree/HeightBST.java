cfa7b0636ebf669a030a4d3121651ede1fc58498
Baljot Malhi <malhibaljotsingh@yahoo.com>
2017-05-26 21:04:50 -0700

    Create HeightBST.java
    Function to find height of Binary Search Tree

---
+++ Data Structures/Binary Tree/HeightBST.java
@@ -0,0 +1,16 @@
+	/*
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	static int height(Node root) {
+      	if(root == null) return -1;
+        
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        
+        if(leftHeight > rightHeight){return leftHeight +1;}
+        else return rightHeight +1;
+        
+    }
